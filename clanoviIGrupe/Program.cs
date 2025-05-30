9388088194375bc39bbe0cf6d5e49f0025c08dc1
Aleksandar Zorkic <aleksandarzorkic@gmail.com>
2025-05-30 10:40:28 +0200

    Dodata je baza podataka kao i .gitignore.

--- clanoviIGrupe/Program.cs
+++ clanoviIGrupe/Program.cs
@@ -1,4 +1,3 @@
-
 namespace clanoviIGrupe
 {
     public class Program
@@ -7,25 +6,32 @@ namespace clanoviIGrupe
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy
+                        .AllowAnyOrigin()   
+                        .AllowAnyHeader()   
+                        .AllowAnyMethod();  
+                });
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
+            app.UseCors("AllowAll");
+
             app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
